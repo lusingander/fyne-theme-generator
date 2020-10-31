6542a8301b7e12f5ab94e8d1117b54e23a927dff
Kyosuke Fujimoto <kyoro.f@gmail.com>
2020-10-31 11:27:34 +0900

    Fix selectors

--- internal/ui/config.go
+++ internal/ui/config.go
@@ -17,6 +17,33 @@ type configPanel struct {
 	parent  fyne.Window
 	current *theme.Setting
 	reflesh func()
+
+	backgroundColorSelector     *colorSelector
+	buttonColorSelector         *colorSelector
+	disabledButtonColorSelector *colorSelector
+	textColorSelector           *colorSelector
+	disabledTextColorSelector   *colorSelector
+	iconColorSelector           *colorSelector
+	disabledIconColorSelector   *colorSelector
+	hyperlinkColorSelector      *colorSelector
+	placeHolderColorSelector    *colorSelector
+	primaryColorSelector        *colorSelector
+	hoverColorSelector          *colorSelector
+	focusColorSelector          *colorSelector
+	scrollBarColorSelector      *colorSelector
+	shadowColorSelector         *colorSelector
+
+	textSizeSelector           *intSelector
+	textFontSelector           *readonlyStringSelector
+	textBoldFontSelector       *readonlyStringSelector
+	textItalicFontSelector     *readonlyStringSelector
+	textBoldItalicFontSelector *readonlyStringSelector
+	textMonospaceFontSelector  *readonlyStringSelector
+
+	paddingSelector            *intSelector
+	iconInlineSizeSelector     *intSelector
+	scrollBarSizeSelector      *intSelector
+	scrollBarSmallSizeSelector *intSelector
 }
 
 func (u *ui) newConfigPanel() *configPanel {
@@ -45,65 +72,81 @@ func (p *configPanel) build() {
 }
 
 func (p *configPanel) configures(ts *theme.Setting) []fyne.CanvasObject {
+	p.backgroundColorSelector = p.newColorSelector(ts.BackgroundColor(), ts.SetBackgroundColor)
+	p.buttonColorSelector = p.newColorSelector(ts.ButtonColor(), ts.SetButtonColor)
+	p.disabledButtonColorSelector = p.newColorSelector(ts.DisabledButtonColor(), ts.SetDisabledButtonColor)
+	p.textColorSelector = p.newColorSelector(ts.TextColor(), ts.SetTextColor)
+	p.disabledTextColorSelector = p.newColorSelector(ts.DisabledTextColor(), ts.SetDisabledTextColor)
+	p.iconColorSelector = p.newColorSelector(ts.IconColor(), ts.SetIconColor)
+	p.disabledIconColorSelector = p.newColorSelector(ts.DisabledIconColor(), ts.SetDisabledIconColor)
+	p.hyperlinkColorSelector = p.newColorSelector(ts.HyperlinkColor(), ts.SetHyperlinkColor)
+	p.placeHolderColorSelector = p.newColorSelector(ts.PlaceHolderColor(), ts.SetPlaceHolderColor)
+	p.primaryColorSelector = p.newColorSelector(ts.PrimaryColor(), ts.SetPrimaryColor)
+	p.hoverColorSelector = p.newColorSelector(ts.HoverColor(), ts.SetHoverColor)
+	p.focusColorSelector = p.newColorSelector(ts.FocusColor(), ts.SetFocusColor)
+	p.scrollBarColorSelector = p.newColorSelector(ts.ScrollBarColor(), ts.SetScrollBarColor)
+	p.shadowColorSelector = p.newColorSelector(ts.ShadowColor(), ts.SetShadowColor)
+	p.textSizeSelector = p.newIntSelector(ts.TextSize(), ts.SetTextSize)
+	p.textFontSelector = p.newReadonlyStringSelector(ts.TextFont().Name())
+	p.textBoldFontSelector = p.newReadonlyStringSelector(ts.TextBoldFont().Name())
+	p.textItalicFontSelector = p.newReadonlyStringSelector(ts.TextItalicFont().Name())
+	p.textBoldItalicFontSelector = p.newReadonlyStringSelector(ts.TextBoldItalicFont().Name())
+	p.textMonospaceFontSelector = p.newReadonlyStringSelector(ts.TextMonospaceFont().Name())
+	p.paddingSelector = p.newIntSelector(ts.Padding(), ts.SetPadding)
+	p.iconInlineSizeSelector = p.newIntSelector(ts.IconInlineSize(), ts.SetIconInlineSize)
+	p.scrollBarSizeSelector = p.newIntSelector(ts.ScrollBarSize(), ts.SetScrollBarSize)
+	p.scrollBarSmallSizeSelector = p.newIntSelector(ts.ScrollBarSmallSize(), ts.SetScrollBarSmallSize)
+
 	cs := make([]fyne.CanvasObject, 0)
-	cs = append(cs, p.colorConfigure("Background color", ts.BackgroundColor(), ts.SetBackgroundColor)...)
-	cs = append(cs, p.colorConfigure("Button color", ts.ButtonColor(), ts.SetButtonColor)...)
-	cs = append(cs, p.colorConfigure("Disable button color", ts.DisabledButtonColor(), ts.SetDisabledButtonColor)...)
-	cs = append(cs, p.colorConfigure("Text color", ts.TextColor(), ts.SetTextColor)...)
-	cs = append(cs, p.colorConfigure("Disable text color", ts.DisabledTextColor(), ts.SetDisabledTextColor)...)
-	cs = append(cs, p.colorConfigure("Icon color", ts.IconColor(), ts.SetIconColor)...)
-	cs = append(cs, p.colorConfigure("Disable icon color", ts.DisabledIconColor(), ts.SetDisabledIconColor)...)
-	cs = append(cs, p.colorConfigure("Hyperlink color", ts.HyperlinkColor(), ts.SetHyperlinkColor)...)
-	cs = append(cs, p.colorConfigure("Placeholder color", ts.PlaceHolderColor(), ts.SetPlaceHolderColor)...)
-	cs = append(cs, p.colorConfigure("Primary color", ts.PrimaryColor(), ts.SetPrimaryColor)...)
-	cs = append(cs, p.colorConfigure("Hover color", ts.HoverColor(), ts.SetHoverColor)...)
-	cs = append(cs, p.colorConfigure("Focus color", ts.FocusColor(), ts.SetFocusColor)...)
-	cs = append(cs, p.colorConfigure("Scroll bar color", ts.ScrollBarColor(), ts.SetScrollBarColor)...)
-	cs = append(cs, p.colorConfigure("Shadow color", ts.ShadowColor(), ts.SetShadowColor)...)
-	cs = append(cs, p.intConfigure("Text size", ts.TextSize(), ts.SetTextSize)...)
-	cs = append(cs, p.readonlyStringConfigure("Text font", ts.TextFont().Name())...)
-	cs = append(cs, p.readonlyStringConfigure("Text bold font", ts.TextBoldFont().Name())...)
-	cs = append(cs, p.readonlyStringConfigure("Text italic font", ts.TextItalicFont().Name())...)
-	cs = append(cs, p.readonlyStringConfigure("Text bold italic font", ts.TextBoldItalicFont().Name())...)
-	cs = append(cs, p.readonlyStringConfigure("Text monospace font", ts.TextMonospaceFont().Name())...)
-	cs = append(cs, p.intConfigure("Padding", ts.Padding(), ts.SetPadding)...)
-	cs = append(cs, p.intConfigure("Icon inline size", ts.IconInlineSize(), ts.SetIconInlineSize)...)
-	cs = append(cs, p.intConfigure("Scroll bar size", ts.ScrollBarSize(), ts.SetScrollBarSize)...)
-	cs = append(cs, p.intConfigure("Scroll bar small size", ts.ScrollBarSmallSize(), ts.SetScrollBarSmallSize)...)
+	cs = append(cs, colorConfigure("Background color", p.backgroundColorSelector)...)
+	cs = append(cs, colorConfigure("Button color", p.buttonColorSelector)...)
+	cs = append(cs, colorConfigure("Disable button color", p.disabledButtonColorSelector)...)
+	cs = append(cs, colorConfigure("Text color", p.textColorSelector)...)
+	cs = append(cs, colorConfigure("Disable text color", p.disabledTextColorSelector)...)
+	cs = append(cs, colorConfigure("Icon color", p.iconColorSelector)...)
+	cs = append(cs, colorConfigure("Disable icon color", p.disabledIconColorSelector)...)
+	cs = append(cs, colorConfigure("Hyperlink color", p.hyperlinkColorSelector)...)
+	cs = append(cs, colorConfigure("Placeholder color", p.placeHolderColorSelector)...)
+	cs = append(cs, colorConfigure("Primary color", p.primaryColorSelector)...)
+	cs = append(cs, colorConfigure("Hover color", p.hoverColorSelector)...)
+	cs = append(cs, colorConfigure("Focus color", p.focusColorSelector)...)
+	cs = append(cs, colorConfigure("Scroll bar color", p.scrollBarColorSelector)...)
+	cs = append(cs, colorConfigure("Shadow color", p.shadowColorSelector)...)
+	cs = append(cs, intConfigure("Text size", p.textSizeSelector)...)
+	cs = append(cs, readonlyStringConfigure("Text font", p.textFontSelector)...)
+	cs = append(cs, readonlyStringConfigure("Text bold font", p.textBoldFontSelector)...)
+	cs = append(cs, readonlyStringConfigure("Text italic font", p.textItalicFontSelector)...)
+	cs = append(cs, readonlyStringConfigure("Text bold italic font", p.textBoldItalicFontSelector)...)
+	cs = append(cs, readonlyStringConfigure("Text monospace font", p.textMonospaceFontSelector)...)
+	cs = append(cs, intConfigure("Padding", p.paddingSelector)...)
+	cs = append(cs, intConfigure("Icon inline size", p.iconInlineSizeSelector)...)
+	cs = append(cs, intConfigure("Scroll bar size", p.scrollBarSizeSelector)...)
+	cs = append(cs, intConfigure("Scroll bar small size", p.scrollBarSmallSizeSelector)...)
 	return cs
 }
 
-func (p *configPanel) colorConfigure(label string, defaultColor color.Color, update func(color.Color)) []fyne.CanvasObject {
-	cs := p.newColorSelector(defaultColor, update)
+func colorConfigure(label string, selector *colorSelector) []fyne.CanvasObject {
 	return []fyne.CanvasObject{
 		widget.NewLabel(label),
 		fyne.NewContainerWithLayout(
 			layout.NewHBoxLayout(),
-			cs.rect,
-			cs.entry,
+			selector.rect,
+			selector.entry,
 		),
 	}
 }
 
-func (p *configPanel) intConfigure(label string, defaultValue int, update func(int)) []fyne.CanvasObject {
-	entry := widget.NewEntry()
-	entry.SetText(strconv.Itoa(defaultValue))
-	entry.OnChanged = func(s string) {
-		if n, err := strconv.Atoi(s); err == nil {
-			update(n)
-			p.reflesh()
-		}
-	}
+func intConfigure(label string, selector *intSelector) []fyne.CanvasObject {
 	return []fyne.CanvasObject{
 		widget.NewLabel(label),
-		entry,
+		selector.entry,
 	}
 }
 
-func (p *configPanel) readonlyStringConfigure(label string, defaultValue string) []fyne.CanvasObject {
+func readonlyStringConfigure(label string, selector *readonlyStringSelector) []fyne.CanvasObject {
 	return []fyne.CanvasObject{
 		widget.NewLabel(label),
-		widget.NewLabel(defaultValue),
+		selector.str,
 	}
 }
 
@@ -159,4 +202,33 @@ func (c *colorSelector) setColor(clr color.Color) {
 func hexColorString(c color.Color) string {
 	rgba := color.RGBAModel.Convert(c).(color.RGBA)
 	return fmt.Sprintf("#%.2X%.2X%.2X%.2X", rgba.R, rgba.G, rgba.B, rgba.A)
+}
+
+type intSelector struct {
+	entry *widget.Entry
+}
+
+func (p *configPanel) newIntSelector(defaultValue int, update func(int)) *intSelector {
+	entry := &widget.Entry{}
+	entry.SetText(strconv.Itoa(defaultValue))
+	entry.OnChanged = func(s string) {
+		if n, err := strconv.Atoi(s); err == nil {
+			update(n)
+			p.reflesh()
+		}
+	}
+	return &intSelector{
+		entry: entry,
+	}
+}
+
+type readonlyStringSelector struct {
+	str *widget.Label
+}
+
+func (p *configPanel) newReadonlyStringSelector(defaultValue string) *readonlyStringSelector {
+	str := widget.NewLabel(defaultValue)
+	return &readonlyStringSelector{
+		str: str,
+	}
 }
