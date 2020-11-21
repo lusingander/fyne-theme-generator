0b64017b3f502faf7646c4507fc6bcf44812dcd3
Kyosuke Fujimoto <kyoro.f@gmail.com>
2020-11-22 07:44:32 +0900

    Add reflesh button

--- internal/ui/toolbar.go
+++ internal/ui/toolbar.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne"
 	"fyne.io/fyne/dialog"
 	"fyne.io/fyne/layout"
+	ft "fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 	"github.com/lusingander/fyne-theme-generator/internal/theme"
 )
@@ -21,16 +22,18 @@ func (u *ui) newToolbarPanel() *toolbarPanel {
 		parent:  u.window,
 		current: u.current,
 	}
-	p.build(u.applyTheme)
+	p.build(u.applyTheme, u.reflesh)
 	return p
 }
 
-func (p *toolbarPanel) build(applyThemeFunc func(fyne.Theme)) {
+func (p *toolbarPanel) build(applyThemeFunc func(fyne.Theme), refleshFunc func()) {
 	themeSelect := widget.NewSelect(theme.EmbeddedThemes, func(string) {})
 	themeSelect.SetSelectedIndex(0)
 	themeApplyButton := widget.NewButton("Apply", func() {
 		applyThemeFunc(theme.GetEmbeddedThemeFrom(themeSelect.Selected))
 	})
+
+	refleshButton := widget.NewButtonWithIcon("", ft.ViewRefreshIcon(), refleshFunc)
 
 	packageNameLabel := widget.NewLabel("Package name:")
 	packageNameEntry := widget.NewEntry()
@@ -56,6 +59,7 @@ func (p *toolbarPanel) build(applyThemeFunc func(fyne.Theme)) {
 		),
 		fyne.NewContainerWithLayout(
 			layout.NewHBoxLayout(),
+			refleshButton,
 			layout.NewSpacer(),
 			packageNameLabel,
 			packageNameEntry,
